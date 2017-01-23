b5e29db907bdaf139ca391e629e8e43776c16944
Tim Andrews <tim.andrews@imagineifinc.com>
2017-01-23 15:26:58 -0500

    Tweek item table, associate user

--- source/stuff/admin.py
+++ source/stuff/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from stuff.models import Item
+
+class itemAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Item
+
+    list_display = [
+        "id",
+        "category",
+        "description",
+    ]
+
+
+
+
+admin.site.register(Item, itemAdmin)
--- source/stuff/models.py
+++ source/stuff/models.py
@@ -1,21 +1,45 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
 
 
 class Item(models.Model):
-    category = models.TextField()
-    location = models.TextField()
-    container = models.TextField()
-    description = models.TextField()
-    unit = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
+    category = models.CharField(max_length=25)
+    location = models.CharField(max_length=25)
+    container = models.CharField(max_length=25, blank=True)
+    description = models.CharField(max_length=100)
+    unit = models.CharField(max_length=25)
     Qty = models.IntegerField()
-    purchaseDate = models.DateTimeField()
-    bestByDate = models.DateTimeField()
-    checkOnFrequency = models.TextField()
-    moreInfoLink = models.TextField()
-    waterStored = models.IntegerField()
-    numServings = models.IntegerField()
-    calPerServing = models.IntegerField()
-    prescriptionMed = models.BooleanField()
+    purchaseDate = models.DateTimeField(blank=True)
+    bestByDate = models.DateTimeField(blank=True)
+    checkOnFrequency = models.CharField(max_length=25, blank=True)
+    moreInfoLink = models.CharField(max_length=100, blank=True)
+    waterStored = models.FloatField(blank=True, null=True)
+    numServings = models.IntegerField(blank=True, null=True)
+    calPerServing = models.IntegerField(blank=True, null=True)
+    prescriptionMed = models.BooleanField(blank=True)
+    notes = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __unicode__(self): # python 2
+        return str(self.id)
+
+    def __str__(self): # python 3
+        return str(self.id)
+
+class Category(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.TextField()
+    sortOrder = models.FloatField()
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __unicode__(self): # python 2
+        return str(self.title)
+
+    def __str__(self): # python 3
+        return str(self.title)
 
