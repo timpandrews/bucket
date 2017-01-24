de5a19a4f8fa6aa42bf21a58e25c1cfb5d5087ff
Tim Andrews <tim.andrews@imagineifinc.com>
2017-01-24 00:11:10 -0500

    Templates & CRUD Views

--- source/stuff/views.py
+++ source/stuff/views.py
@@ -4,25 +4,25 @@ def stuff_list(request):
     context = {
         "title": "list"
     }
-    return render(request, "home.html", context)
+    return render(request, "stuff_list.html", context)
 
 def stuff_create(request):
     context = {
         "title": "create"
     }
-    return render(request, "home.html", context)
+    return render(request, "stuff_form.html", context)
 
 def stuff_update(request, id=None):
     context = {
         "title": "update"
     }
-    return render(request, "home.html", context)
+    return render(request, "stuff_form.html", context)
 
 def stuff_detail(request, id=None):
     context = {
         "title": "detail"
     }
-    return render(request, "home.html", context)
+    return render(request, "stuff_detail.html", context)
 
 def stuff_delete(request, id=None):
     context = {
